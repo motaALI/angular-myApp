8ddd1691ffcfc54dcf14acc0a9cbb579c93ef1cf
motaALI <motawakil.ali.90@gmail.com>
2020-03-25 17:09:37 +0100

    C architecteur and input/output + immutable state change + onchang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,13 @@
 import { Component } from '@angular/core';
 
-interface Passenger {
-  id: number,
-  fullname: string;
-  checkedIn: boolean;
-  checkInDate?: number; // added for worke with Pipes
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true,
-    checkInDate: 1490742000000
-    },
-    {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false
-    },
-    {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1491606000000
-    },
-    {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1488412800000
-    },
-    {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false
-    }];
+ 
   title = 'gealgaded';
-  numberOne = 1;
-  numberTwo = 2;
-  isHappy = true;
-  name: string = 'Mota'
 
-  handleBlur(event: any) {
-    this.name = event.target.value;
-    console.log(event);
-  }
-  handleInput(event: any) {
-    this.name = event.target.value;
-  }
-
-  handleChange(value: string) {
-    this.name = value;
-  }
-  handleClick(value: string) {
-    this.name = value;
-    console.log(value);
-  }
 
 }
