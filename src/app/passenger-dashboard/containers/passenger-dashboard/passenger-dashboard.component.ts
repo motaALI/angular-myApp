6607b7faf434753256fd4477c461ca7b05a48900
motaALI <motawakil.ali.90@gmail.com>
2020-03-27 14:28:05 +0100

    servie workout with httpAnd customHeaders and promises

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -2,68 +2,74 @@ import { Component, OnInit } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 
+import { PassengerDashboardService } from '../../passenger-dashboard.service';
+
 
 @Component({
   selector: 'passenger-dashboard',
   templateUrl: './passenger-dashboard.component.html',
   styleUrls: ['./passenger-dashboard.component.css']
 })
+
+
 export class PassengerDashboardComponent implements OnInit {
 
   passengers: Passenger[];
-  constructor() {}
+
+  constructor(
+    private passengerService: PassengerDashboardService
+    ) {}
+
+
   ngOnInit() {
-    this.passengers = [{
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: [{name: 'TotoTATA', age: 3}]
-      },
-      {
-        id: 2,
-        fullname: 'Rose',
-        checkedIn: false,
-        children: [{name: 'Toto', age: 11}, {name: 'Tata', age: 7},]
-      },
-      {
-        id: 3,
-        fullname: 'James',
-        checkedIn: true,
-        checkInDate: 1491606000000,
-        children: null
-      },
-      {
-        id: 4,
-        fullname: 'Louise',
-        checkedIn: true,
-        checkInDate: 1488412800000,
-        children: [{name: 'Test', age: 4}, {name: 'Titi', age: 5},]
-      },
-      {
-        id: 5,
-        fullname: 'Tina',
-        checkedIn: false,
-        children: null
-      }];
+   /* this.passengerService
+       .getPassengers()
+       .subscribe((data: Passenger[]) => {
+         console.log('Data'+ data);
+         this.passengers = data;
+       });*/ // problem with db locally 
+      this.passengerService.getJSON().subscribe(data => {
+        this.passengers = data.passengers;
+    });   
   }
 
   handleRemove(event: Passenger) {
-    console.log(event);
-    this.passengers = this.passengers.filter((passenger: Passenger) => {
-      return passenger.id !== event.id;
-    })
+    // console.log(event);
+    this.passengerService
+    .removePassenger(event)
+    .subscribe((data: Passenger) => {
+      this.passengers = this.passengers.filter((passenger: Passenger) => {
+        return passenger.id !== event.id;
+      })
+    });
+    
   }
   handleEdit(event: Passenger) {
     console.log(event);
-    this.passengers = this.passengers.map((passenger: Passenger) => {
-      if (passenger.id === event.id) {
-        passenger = Object.assign({}, passenger, event);
-      }
-      return passenger;
-    });
-    console.log(this.passengers);
+    this.passengerService
+      .udatePassenger(event)
+      .subscribe((data:Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+        });
+      });
   }
 
+  handleEditUsePromis(event: Passenger) {
+    console.log(event);
+    this.passengerService
+      .udatePassengerUsePromise(event)
+      .then((data:Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+        });
+      });
+    }
 
 }
